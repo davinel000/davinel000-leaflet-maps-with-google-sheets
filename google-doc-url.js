7d21f6ea9758d4e907f2399c97d17a68acafa08e
davinel000 <davinel000@gmail.com>
2018-06-27 23:12:15 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1WX3ou0Us7EPoSc1paQhRo1zrPuO1hFVzoF6ldw2NWcs/edit#gid=0';
